56dd9dc0871027026045f07e47a7e1064320e7d8
AdityaKevin <adityakevin50@gmail.com>
2019-04-27 02:00:43 +0000

    Revert "Revert "land: Update ims.apk and qti-telephony-common.jar from daisy""
    This reverts commit 499e9a64c16e2691a58e3705e00e23b060aaa2fd.

--- land/land-vendor.mk
+++ land/land-vendor.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -1617,12 +1617,11 @@ PRODUCT_PACKAGES += \
     libthermalclient \
     libtime_genoff \
     QtiTelephonyService \
-    ims \
-    imssettings \
     uceShimService \
     CNEService \
     WfdService \
     com.qualcomm.location \
+    ims \
     qcrilmsgtunnel \
     PowerOffAlarm \
     TimeService \
